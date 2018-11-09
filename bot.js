0d45b2d424c4d105dc59074238bd8987943dbdd8
newmusica <44785626+newmusica@users.noreply.github.com>
2018-11-09 19:15:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,48 +29,38 @@ bot.on('message', function(message) {
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         bot.user.setUsername(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
+
         });
 } else if(message.content.startsWith(prefix + "stream")) {
                 if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         bot.user.setGame(args , 'https://twitch.tv/6xlez1');
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
+
         });
     } else if(message.content.startsWith(prefix + "ply")) {
                         if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         bot.user.setGame(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "listen")) {
                         if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         bot.user.setActivity(args, {type:'LISTENING'});
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "watch")) {
                         if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         bot.user.setActivity(args, {type:'WATCHING'});
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "setavatar")) {
                         if(message.author.id !== myID) return;
         bot.user.setAvatar(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
                 if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-           msg.delete(5000);
-          message.delete(5000);
         });
     }
 });
